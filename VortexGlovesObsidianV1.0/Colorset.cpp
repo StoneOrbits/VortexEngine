be4358aa8eb3372d34c9525e0f5f194a67a7dea4
Dan <UnrealDanMail@gmail.com>
2022-04-16 01:30:23 -0700

    minor change

--- VortexGlovesObsidianV1.0/Colorset.cpp
+++ VortexGlovesObsidianV1.0/Colorset.cpp
@@ -1,3 +1,17 @@
 #include "Colorset.h"
 
+Colorset::Colorset()
+{
+}
 
+// create a new colorset with a list of colors
+bool Colorset::init(uint32_t amount, HSVColor *colors)
+{
+  return true;
+}
+
+// get a color from the colorset
+HSVColor get(uint32_t index)
+{
+  return HSVColor(0, 0, 0);
+}
