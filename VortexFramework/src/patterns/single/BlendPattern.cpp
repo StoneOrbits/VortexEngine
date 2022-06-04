6d27605906953d64dd6ea057bd40ab46f9395e7c
Dan <UnrealDanMail@gmail.com>
2022-06-04 15:58:41 -0700

    Some fixes for blend

--- VortexFramework/src/patterns/single/BlendPattern.cpp
+++ VortexFramework/src/patterns/single/BlendPattern.cpp
@@ -6,6 +6,8 @@
 #include "../../Leds.h"
 #include "../../Log.h"
 
+#include <math.h>
+
 BlendPattern::BlendPattern(uint8_t onDuration, uint8_t offDuration, uint8_t blendSpeed) :
   BasicPattern(onDuration, offDuration),
   m_speed(blendSpeed),
@@ -34,7 +36,6 @@ void BlendPattern::play()
 
 void BlendPattern::onBlinkOn()
 {
-  int32_t hueDiff = 0;
   // if the current hue has reached the next hue
   if (m_cur.hue == m_next.hue) {
     // copy over the sat/val
@@ -42,14 +43,14 @@ void BlendPattern::onBlinkOn()
     // get the next color and convert to hsv
     m_next = m_colorset.getNext();
   }
-  // if the next color is greater than current in hue
-  if (m_next.hue > m_cur.hue) {
-    // increment current hue
-    m_cur.hue += m_speed;
-  } else {
-    // otherwise decrement current hue
-    m_cur.hue -= m_speed;
-  }
+  // check which direction is closer for the next hue
+  int sign = (abs(m_next.hue - m_cur.hue) < 128) ? 1 : -1;
+  // move hue in the direction of next hue at chosen speed
+  // NOTE: if the speed isn't a multiple of the hue values then
+  //       it will cause oscillation around the target hue 
+  //       because it will never reach the target hue and 
+  //       always over/under shoot
+  m_cur.hue += m_speed * sign;
   // set the target led with the current HSV color
   Leds::setIndex(m_ledPos, m_cur);
 }
