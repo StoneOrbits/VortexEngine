060294564624030a9032655c2d7d8ed34b084ca8
Dan <UnrealDanMail@gmail.com>
2022-08-05 16:48:48 -0700

    zigzag work

--- VortexEngine/src/patterns/multi/ZigzagPattern.h
+++ VortexEngine/src/patterns/multi/ZigzagPattern.h
@@ -14,7 +14,7 @@
 class ZigzagPattern : public MultiLedPattern
 {
 public:
-  ZigzagPattern(uint8_t stepDuration = 50, bool fade = false);
+  ZigzagPattern(uint8_t stepDuration = 50, uint8_t snakeSize = 1, uint8_t fadeAmount = 55);
   virtual ~ZigzagPattern();
 
   // init the pattern to initial state
@@ -29,13 +29,15 @@ public:
 
 private:
   void drawSnake(uint32_t size, uint8_t step, uint32_t colIndex);
+  void drawSnakeSegment(uint8_t step, uint32_t colIndex, uint32_t fadeby);
 
   // path for leds to take, index this with m_step up to LED_COUNT steps
   static const LedPos ledStepPositions[NUM_ZIGZAG_STEPS];
 
-  bool m_fade;
-  uint8_t m_step;
   uint8_t m_stepDuration;
+  uint8_t m_snakeSize;
+  uint8_t m_fadeAmount;
+  uint8_t m_step;
 
   Timer m_blinkTimer;
   Timer m_stepTimer;
