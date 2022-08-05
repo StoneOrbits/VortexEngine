4f761f52b8e67ee475c08413858024afa0ee3aa6
Shane Aronson <LivingSynthesis@gmail.com>
2022-08-05 10:44:24 -0700

    Added Zipfade to the list of patterns

--- VortexEngine/src/patterns/multi/ZigzagPattern.cpp
+++ VortexEngine/src/patterns/multi/ZigzagPattern.cpp
@@ -33,6 +33,7 @@ ZigzagPattern::ZigzagPattern(bool fade) :
   MultiLedPattern(),
   m_fade(fade),
   m_step(FINGER_FIRST),
+  m_stepDuration(stepDuration),
   m_blinkTimer(),
   m_stepTimer()
 {
@@ -57,7 +58,7 @@ void ZigzagPattern::init()
 
   // reset and add alarm
   m_stepTimer.reset();
-  m_stepTimer.addAlarm(50);
+  m_stepTimer.addAlarm(m_stepDuration);
   m_stepTimer.start();
 
   // start on the first color so that cur() works immediately
