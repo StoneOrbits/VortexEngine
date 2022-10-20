9a94abf739c83fed9def15961e8f35b3c18a7083
Shane Aronson <LivingSynthesis@gmail.com>
2022-10-19 18:12:49 -0700

    Expose Backstrobe parameter

--- VortexEngine/src/patterns/multi/BackStrobePattern.cpp
+++ VortexEngine/src/patterns/multi/BackStrobePattern.cpp
@@ -3,8 +3,9 @@
 #include "../../PatternBuilder.h"
 #include "../../Colorset.h"
 
-BackStrobePattern::BackStrobePattern() :
+BackStrobePattern::BackStrobePattern(uint8_t stepSpeed) :
   HybridPattern(),
+  m_stepSpeed(stepSpeed),
   m_stepTimer(),
   m_switch()
 {
@@ -21,7 +22,7 @@ void BackStrobePattern::init()
 
   // timer for switch
   m_stepTimer.reset();
-  m_stepTimer.addAlarm(1000);
+  m_stepTimer.addAlarm(m_stepSpeed);
   m_stepTimer.start();
 }
 
@@ -46,4 +47,15 @@ void BackStrobePattern::play()
     }
   }
   HybridPattern::play();
+}
+
+// must override the serialize routine to save the pattern
+void BackStrobePattern::serialize(SerialBuffer& buffer) const
+{
+  HybridPattern::serialize(buffer);
+}
+
+void BackStrobePattern::unserialize(SerialBuffer& buffer)
+{
+  HybridPattern::unserialize(buffer);
 }
--- VortexEngine/src/patterns/multi/BackStrobePattern.h
+++ VortexEngine/src/patterns/multi/BackStrobePattern.h
@@ -7,14 +7,19 @@
 class BackStrobePattern : public HybridPattern
 {
 public:
-  BackStrobePattern();
+  BackStrobePattern(uint8_t switchSpeed = 1000);
   virtual ~BackStrobePattern();
 
   // init the pattern to initial state
   virtual void init() override;
   virtual void play() override;
 
+  // must override the serialize routine to save the pattern
+  virtual void serialize(SerialBuffer& buffer) const override;
+  virtual void unserialize(SerialBuffer& buffer) override;
+
 private:
+  uint8_t m_stepSpeed;
   // the step timer
   Timer m_stepTimer;
 
