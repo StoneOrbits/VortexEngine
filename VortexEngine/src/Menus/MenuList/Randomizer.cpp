1c2b03045f481273c7b11b9299e9e5ff1dbc647b
Dan <UnrealDanMail@gmail.com>
2022-12-02 17:07:52 -0800

    Fixed randomizer color

--- VortexEngine/src/Menus/MenuList/Randomizer.cpp
+++ VortexEngine/src/Menus/MenuList/Randomizer.cpp
@@ -51,7 +51,7 @@ bool Randomizer::run()
   m_pRandomizedMode->play();
 
   if (g_pButton->isPressed() && g_pButton->holdDuration() > SHORT_CLICK_THRESHOLD_TICKS) {
-    Leds::setAll(RGB_GREEN);
+    Leds::setAll(RGB_DIM_WHITE);
   }
 
   // return true to continue staying in randomizer menu
