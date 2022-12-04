8bf47f1a2246373860a2c6b9c9c68f7383b345a3
Dan <UnrealDanMail@gmail.com>
2022-12-04 00:45:27 -0800

    Fixes to factory reset

--- VortexEngine/src/Menus/MenuList/FactoryReset.cpp
+++ VortexEngine/src/Menus/MenuList/FactoryReset.cpp
@@ -56,7 +56,8 @@ void FactoryReset::onLongClick()
     leaveMenu();
     return;
   case FINGER_COUNT:
-    if (g_pButton->holdDuration() > FACTORY_RESET_THRESHOLD_TICKS) {
+    // must wait till all empty + one white blink
+    if (g_pButton->holdDuration() > (FACTORY_RESET_THRESHOLD_TICKS + Time::msToTicks(700))) {
       Modes::setDefaults();
       leaveMenu(true);
     }
@@ -66,25 +67,46 @@ void FactoryReset::onLongClick()
 
 void FactoryReset::showReset()
 {
+  // if we're on the thumb just set the rest to blank
   if (m_curSelection == FINGER_THUMB) {
     Leds::setRange(LED_FIRST, INDEX_TOP, RGB_BLANK);
     return;
   }
-  if (g_pButton->isPressed()) {
-    Leds::setRange(LED_FIRST, INDEX_TOP, RGB_BLANK);
-    uint32_t duration = (g_pButton->holdDuration() * INDEX_TOP) / FACTORY_RESET_THRESHOLD_TICKS;
-    if (duration <= INDEX_TOP) {
-      int8_t hue = 20 - (4 * duration);
-      if (hue < 0) {
-        hue = 0;
-      }
-      Leds::blinkRange(LED_FIRST, (LedPos)(INDEX_TOP - duration), Time::getCurtime(), 
-        150 - (12 * duration), 200 - (10 * duration), HSVColor(hue, 255, 180));
-    } else {
-      Leds::blinkRange(LED_FIRST, INDEX_TOP, Time::getCurtime(), 80, 60, RGB_WHITE);
-    }
-  } else {
+  // otherwise we're not on thumb, if the button isn't pressed
+  if (!g_pButton->isPressed()) {
+    // just idle blink from clear to blank
     Leds::clearRange(LED_FIRST, INDEX_TOP);
     Leds::blinkRange(LED_FIRST, INDEX_TOP, Time::getCurtime(), 250, 150, RGB_BLANK);
+    return;
   }
+
+  // the button is pressed so show the reset countdown timer
+
+  // the progress is how long the hold duration has been held
+  // relative to the factory reset threshold time
+  float progress = (float)g_pButton->holdDuration() / FACTORY_RESET_THRESHOLD_TICKS;
+  // the ledProgress is just an LED from pinky tip to index top based on progress
+  LedPos ledProgress = (LedPos)(progress * INDEX_TOP);
+  // max the led progress at index top (don't include thumb)
+  if (ledProgress > INDEX_TOP) {
+    // when we reach the end of the progress bar just blink white
+    Leds::blinkRange(LED_FIRST, INDEX_TOP, Time::getCurtime(), 80, 60, RGB_WHITE);
+    return;
+  }
+
+  // the off/on ms blink faster based on the progress
+  uint32_t offMs = 150 - (12 * ledProgress);
+  uint32_t onMs = 200 - (10 * ledProgress);
+  // the hue gets more red starting at 20 going in steps of 4 to min 0
+  int8_t hue = (ledProgress < 6) ? 20 - (4 * ledProgress) : 0;
+  // the 'endled' is the tip of the reset progress bar, since the progress
+  // bar starts full red and empties down to the pinky that means it is
+  // inverted from the 'ledProgress' which starts at 0 and grows
+  LedPos endLed = (LedPos)(INDEX_TOP - ledProgress);
+  // clear all the leds so that 'blinkRange' will blink from off to the designated color
+  Leds::clearRange(LED_FIRST, INDEX_TOP);
+  // blink to the calculated redish hue from pinky to the end led
+  Leds::blinkRange(LED_FIRST, endLed, Time::getCurtime(), offMs, onMs, HSVColor(hue, 255, 180));
+  // and blink the background the regular blank color
+  Leds::blinkRange((LedPos)(endLed + 1), INDEX_TOP, Time::getCurtime(), offMs, onMs, RGB_BLANK);
 }
