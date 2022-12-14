9fa1be989ba38d06d1d7229d4ff75d7d8b60540e
Dan <UnrealDanMail@gmail.com>
2022-12-14 11:29:38 -0800

    another minor fix

--- VortexEngine/src/Patterns/Multi/ImpactPattern.cpp
+++ VortexEngine/src/Patterns/Multi/ImpactPattern.cpp
@@ -23,20 +23,20 @@ void ImpactPattern::init()
   HybridPattern::init();
 
   // thumb, middle and index/ring/pinkie
-  Colorset thumb_set(m_colorset.get(0));
-  Colorset middle_set(m_colorset.get(1));
-  Colorset third_set(m_colorset.get(2));
+  Colorset thumbSet(m_colorset.get(0));
+  Colorset middleSet(m_colorset.get(1));
+  Colorset thirdSet(m_colorset.get(2));
   
   PatternArgs strobeArgs(25, 250);
-  setPatternAt(THUMB_TIP, PATTERN_BASIC, &strobeArgs, &thumb_set);
-  setPatternAt(THUMB_TOP, PATTERN_BASIC, &strobeArgs, &thumb_set);
+  setPatternAt(THUMB_TIP, PATTERN_BASIC, &strobeArgs, &thumbSet);
+  setPatternAt(THUMB_TOP, PATTERN_BASIC, &strobeArgs, &thumbSet);
 
-  setPatternAt(MIDDLE_TIP, PATTERN_BASIC, &strobeArgs, &middle_set);
-  setPatternAt(MIDDLE_TOP, PATTERN_BASIC, &strobeArgs, &middle_set);
+  setPatternAt(MIDDLE_TIP, PATTERN_BASIC, &strobeArgs, &middleSet);
+  setPatternAt(MIDDLE_TOP, PATTERN_BASIC, &strobeArgs, &middleSet);
 
-  setPatternAt(INDEX_TOP, PATTERN_BASIC, &strobeArgs, &third_set);
-  setPatternAt(RING_TOP, PATTERN_BASIC, &strobeArgs, &third_set);
-  setPatternAt(PINKIE_TOP, PATTERN_BASIC, &strobeArgs, &third_set);
+  setPatternAt(INDEX_TOP, PATTERN_BASIC, &strobeArgs, &thirdSet);
+  setPatternAt(RING_TOP, PATTERN_BASIC, &strobeArgs, &thirdSet);
+  setPatternAt(PINKIE_TOP, PATTERN_BASIC, &strobeArgs, &thirdSet);
 
   // remaining just use default colorset
   setPatternAt(INDEX_TIP, PATTERN_STROBE);
