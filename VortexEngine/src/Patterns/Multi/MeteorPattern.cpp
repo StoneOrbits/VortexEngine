5e14a21ba4a571707a882071f7ce9c9727d51551
Shane Aronson <LivingSynthesis@gmail.com>
2023-01-11 19:02:32 -0800

    Fixes for patterns using fades

--- VortexEngine/src/Patterns/Multi/MeteorPattern.cpp
+++ VortexEngine/src/Patterns/Multi/MeteorPattern.cpp
@@ -29,6 +29,8 @@ void MeteorPattern::blinkOn()
     m_stash[i].adjustBrightness(m_fadeAmount);
   }
   Leds::restoreAll(m_stash);
+  Leds::adjustBrightnessAll(m_fadeAmount);
+  Leds::stashAll(m_stash);
 }
 
 void MeteorPattern::blinkOff()
