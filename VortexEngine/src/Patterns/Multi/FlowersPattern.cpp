866e3043a7dd45d0425d3a1c7c9fd0dbfd4a563e
Dan <UnrealDanMail@gmail.com>
2023-01-30 16:47:20 -0800

    Fix for flowers

--- VortexEngine/src/Patterns/Multi/FlowersPattern.cpp
+++ VortexEngine/src/Patterns/Multi/FlowersPattern.cpp
@@ -29,40 +29,19 @@ void FlowersPattern::init()
   HybridPattern::init();
 
   // split colorset
-  Colorset sideSet(m_colorset.get(0), m_colorset.get(1));
-  Colorset midSet(m_colorset.get(2), m_colorset.get(3));
+  Colorset set1(m_colorset.get(0), m_colorset.get(1));
+  Colorset set2(m_colorset.get(2), m_colorset.get(3));
 
   // quadrant 1 and 3 use duration 1
-  setPatternAt(LED_0, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_1, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_2, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_3, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_4, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_5, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_6, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_14, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_15, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_16, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_17, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_18, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_19, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-  setPatternAt(LED_20, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &sideSet);
-
-  // quadrant 2 and 4 use duration 2
-  setPatternAt(LED_7, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_8, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_9, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_10, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_11, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_12, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_13, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_21, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_22, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_23, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_24, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_25, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_26, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
-  setPatternAt(LED_27, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &midSet);
+  for (LedPos led = LED_FIRST; led < LED_COUNT; ++led) {
+    if (led < LED_7 || (led > LED_13 && led < LED_21)) {
+      // 1-6 and 14-20
+      setPatternAt(led, PatternBuilder::makeSingle(PATTERN_BASIC, &m_firstPatternArgs), &set1);
+    } else {
+      // 7-13 and 21-27
+      setPatternAt(led, PatternBuilder::makeSingle(PATTERN_BASIC, &m_secondPatternArgs), &set2);
+    }
+  }
 }
 
 void FlowersPattern::setArgs(const PatternArgs &args)
