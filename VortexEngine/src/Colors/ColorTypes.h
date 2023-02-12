eb16d4af198e204fdea39f1f39194ac96b0e7d16
Dan <UnrealDanMail@gmail.com>
2023-02-11 19:42:17 -0800

    Some fixes

--- VortexEngine/src/Colors/ColorTypes.h
+++ VortexEngine/src/Colors/ColorTypes.h
@@ -10,30 +10,34 @@
 //#define HSV(h, s, v) (HSV_BIT | (h << 16) | (s << 8) | v)
 
 // Pre defined hex HSV values
-#define HSV_WHITE   0 // HSV_BIT | 0x00006E    // 0 0 110
-#define HSV_ORANGE  0 // HSV_BIT | 0x14FF6E    // 20 255 110
-#define HSV_BLUE    0 // HSV_BIT | 0xA0FF6E    // 160 255 110
-#define HSV_YELLOW  0 // HSV_BIT | 0x3CFF6E    // 60  255 110
-#define HSV_RED     0 // HSV_BIT | 0x00FF6E    // 0   255 110
-#define HSV_GREEN   0 // HSV_BIT | 0x55FF6E    // 85  255 110
-#define HSV_TEAL    0 // HSV_BIT | 0x78FF6E    // 120 255 110
-#define HSV_PURPLE  0 // HSV_BIT | 0xD4FF6E    // 212 255 110
-#define HSV_BLANK   0 // HSV_BIT | 0x000040    //   0   0  40
+#ifdef VORTEX_LIB
+#define HSV_WHITE   HSV_BIT | 0x00006E    // 0 0 110
+#define HSV_ORANGE  HSV_BIT | 0x14FF6E    // 20 255 110
+#define HSV_BLUE    HSV_BIT | 0xA0FF6E    // 160 255 110
+#define HSV_YELLOW  HSV_BIT | 0x3CFF6E    // 60  255 110
+#define HSV_RED     HSV_BIT | 0x00FF6E    // 0   255 110
+#define HSV_GREEN   HSV_BIT | 0x55FF6E    // 85  255 110
+#define HSV_TEAL    HSV_BIT | 0x78FF6E    // 120 255 110
+#define HSV_PURPLE  HSV_BIT | 0xD4FF6E    // 212 255 110
+#define HSV_BLANK   HSV_BIT | 0x000040    //   0   0  40
+#endif
 #define HSV_OFF     HSVColor() // HSV_BIT | 0x000000    //   0   0   0
 
-#define RGB_WHITE       0 // 0xFFFFFF    // 170 170 170
-#define RGB_DIM_WHITE1  0 // 0xaaaaaa    // 170 170 170
-#define RGB_DIM_WHITE2  0 // 0x787878    // 120 120 120
-#define RGB_ORANGE      0 // 0x832300
-#define RGB_BLUE        0 // 0x0000FF    //   0   0 255
-#define RGB_YELLOW      0 // 0xFFFF00    //
-#define RGB_RED         0 // 0xFF0000    //   0 255 110
-#define RGB_DARK_RED    0 // 0x110000    //   0 255 110
-#define RGB_GREEN       0 // 0x00FF00    //  85 255 110
-#define RGB_CYAN        0 // 0x00FFFF    //   0 255 255
-#define RGB_TEAL        0 // 0x00FF80    //
-#define RGB_PURPLE      0 // 0x9933FF    //
-#define RGB_BLANK       0 // 0x101010    //  16  16  16
+#ifdef VORTEX_LIB
+#define RGB_WHITE       0xFFFFFF    // 170 170 170
+#define RGB_DIM_WHITE1  0xaaaaaa    // 170 170 170
+#define RGB_DIM_WHITE2  0x787878    // 120 120 120
+#define RGB_ORANGE      0x832300
+#define RGB_BLUE        0x0000FF    //   0   0 255
+#define RGB_YELLOW      0xFFFF00    //
+#define RGB_RED         0xFF0000    //   0 255 110
+#define RGB_DARK_RED    0x110000    //   0 255 110
+#define RGB_GREEN       0x00FF00    //  85 255 110
+#define RGB_CYAN        0x00FFFF    //   0 255 255
+#define RGB_TEAL        0x00FF80    //
+#define RGB_PURPLE      0x9933FF    //
+#define RGB_BLANK       0x101010    //  16  16  16
+#endif
 #define RGB_OFF         RGBColor() // 0x000000    //   0   0   0
 
 // Some Pre-defined hue values
@@ -71,6 +75,11 @@ public:
   bool empty() const;
   void clear();
 
+#ifdef VORTEX_LIB
+  // can't do this on arduino 8bit
+  uint32_t raw() const { return HSV_BIT | (hue << 16) | (sat << 8) | val; }
+#endif
+
   // public members
   uint8_t hue;
   uint8_t sat;
@@ -101,6 +110,11 @@ public:
   void adjustBrightness(uint8_t fadeBy);
   void serialize(ByteStream &buffer) const;
   void unserialize(ByteStream &buffer);
+
+#ifdef VORTEX_LIB
+  // can't do this on arduino 8bit
+  uint32_t raw() const { return (red << 16) | (green << 8) | blue; }
+#endif
 
   // public members
   uint8_t red;
