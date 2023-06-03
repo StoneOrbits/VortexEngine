61b55314164f5baa24dde8cf252fd08446cf508b
Dan <UnrealDanMail@gmail.com>
2023-06-03 04:34:30 -0700

    comment

--- VortexEngine/src/VortexEngine.cpp
+++ VortexEngine/src/VortexEngine.cpp
@@ -144,6 +144,8 @@ void VortexEngine::runMainLogic()
       // go back to sleep if they don't unlock in time
       enterSleep();
     }
+    // OPTIONAL: render a dim led during unlock window waiting for clicks?
+    // Leds::setIndex(LED_1, RGB_DIM_RED);
     // don't do anything else while locked, just return
     return;
   }
