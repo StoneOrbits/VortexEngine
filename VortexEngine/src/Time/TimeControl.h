c56dfa7d478091fd26c4a6ab0bd7d58ef723beac
Dan <UnrealDanMail@gmail.com>
2023-06-20 20:38:21 -0700

    comment fix

--- VortexEngine/src/Time/TimeControl.h
+++ VortexEngine/src/Time/TimeControl.h
@@ -108,6 +108,8 @@ private:
 #endif
 };
 
+// TODO: properly move these apis into here so that VortexLib doesn't
+//       need to provide them somehow
 void delayMicroseconds(uint16_t us);
 void delay(uint16_t ms);
 unsigned long micros();
