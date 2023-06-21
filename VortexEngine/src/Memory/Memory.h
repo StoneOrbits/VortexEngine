4263529e00a2fda71ad1f5edc476ee6230ddf914
Dan <UnrealDanMail@gmail.com>
2023-06-21 04:47:41 -0700

    Fixes for wireless

--- VortexEngine/src/Memory/Memory.h
+++ VortexEngine/src/Memory/Memory.h
@@ -45,12 +45,12 @@ void *operator new  (size_t size, void *ptr) noexcept;
 void *operator new[](size_t size, void *ptr) noexcept;
 void  operator delete  (void *ptr, size_t size) noexcept;
 void  operator delete[](void *ptr, size_t size) noexcept;
-void *operator new  (size_t size, std::align_val_t al);
-void *operator new[](size_t size, std::align_val_t al);
-void  operator delete  (void *ptr, std::align_val_t al) noexcept;
-void  operator delete[](void *ptr, std::align_val_t al) noexcept;
-void  operator delete  (void *ptr, size_t size, std::align_val_t al) noexcept;
-void  operator delete[](void *ptr, size_t size, std::align_val_t al) noexcept;
+//void *operator new  (size_t size, std::align_val_t al);
+//void *operator new[](size_t size, std::align_val_t al);
+//void  operator delete  (void *ptr, std::align_val_t al) noexcept;
+//void  operator delete[](void *ptr, std::align_val_t al) noexcept;
+//void  operator delete  (void *ptr, size_t size, std::align_val_t al) noexcept;
+//void  operator delete[](void *ptr, size_t size, std::align_val_t al) noexcept;
 #endif
 
 #endif
