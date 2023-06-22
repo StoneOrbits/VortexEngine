eeb5a0762d715689018ab58c8009d73d4dc469aa
Dan <UnrealDanMail@gmail.com>
2023-06-21 17:11:40 -0700

    first attempt at lifi from orbit

--- VortexEngine/src/Menus/MenuList/ModeSharing.h
+++ VortexEngine/src/Menus/MenuList/ModeSharing.h
@@ -14,13 +14,12 @@ public:
 
   // handlers for clicks
   void onShortClick() override;
-  void onShortClick2() override;
   void onLongClick() override;
-  void onLongClick2() override;
 
 private:
   void beginSending();
   void continueSending();
+  void beginReceiving();
   void receiveMode();
 
   void showSendMode();
@@ -32,8 +31,6 @@ private:
   };
 
   ModeShareState m_sharingMode;
-  // last time data was sent
-  uint64_t m_lastActionTime;
 
   // the start time when checking for timing out
   uint64_t m_timeOutStartTime;
