fefb2a8a81fa2e92ba1c52fa03e40a72bff36208
Dan <UnrealDanMail@gmail.com>
2023-07-10 19:04:55 -0700

    more fixes

--- VortexEngine/src/Menus/MenuList/PatternSelect.cpp
+++ VortexEngine/src/Menus/MenuList/PatternSelect.cpp
@@ -50,10 +50,10 @@ void PatternSelect::onLedSelected()
 void PatternSelect::onShortClick()
 {
   LedPos srcLed = LED_MULTI;
-  if (!m_patternMode.isMultiLed()) {
+  if (!m_previewMode.isMultiLed()) {
     srcLed = mapGetFirstLed(m_targetLeds);
   }
-  PatternID newID = (PatternID)(m_patternMode.getPatternID(srcLed) + 1);
+  PatternID newID = (PatternID)(m_previewMode.getPatternID(srcLed) + 1);
   PatternID endList = PATTERN_SINGLE_LAST;
   PatternID beginList = PATTERN_SINGLE_FIRST;
 #if VORTEX_SLIM == 0
