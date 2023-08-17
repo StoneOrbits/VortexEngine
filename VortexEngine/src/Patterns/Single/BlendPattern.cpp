d00a57069e2954c57dd5d1d396dedb14ae738573
Dan <UnrealDanMail@gmail.com>
2023-08-16 23:37:46 -0700

    Changes to replace hueshift with real blend

--- VortexEngine/src/Patterns/Single/BlendPattern.cpp
+++ VortexEngine/src/Patterns/Single/BlendPattern.cpp
@@ -8,17 +8,13 @@ BlendPattern::BlendPattern(const PatternArgs &args) :
   BasicPattern(args),
   m_blendSpeed(0),
   m_numFlips(0),
-  m_hueShift(0),
-  m_curHSV(),
-  m_nextHSV(),
-  m_curRGB(),
-  m_nextRGB(),
+  m_cur(),
+  m_next(),
   m_flip(0)
 {
   m_patternID = PATTERN_BLEND;
   REGISTER_ARG(m_blendSpeed);
   REGISTER_ARG(m_numFlips);
-  REGISTER_ARG(m_hueShift);
   setArgs(args);
 }
 
@@ -31,69 +27,49 @@ void BlendPattern::init()
   // run basic pattern init logic
   BasicPattern::init();
   // convert current/next colors to HSV
-  m_curHSV = m_curRGB = m_colorset.getNext();
-  m_nextHSV = m_nextRGB = m_colorset.getNext();
+  m_cur = m_colorset.getNext();
+  m_next = m_colorset.getNext();
   // reset the flip count
   m_flip = 0;
 }
 
 void BlendPattern::onBlinkOn()
 {
-  bool timeToTick = ((Time::getCurtime() % 4) == 0);
-  if (timeToTick) {
-    if (m_hueShift) {
-      if (m_curHSV == m_nextHSV) {
-        m_nextHSV = m_colorset.getNext();
+  if ((Time::getCurtime() % 4) == 0) {
+      if (m_cur == m_next) {
+        m_next = m_colorset.getNext();
       }
-      interpolate(m_curHSV.hue, m_nextHSV.hue, INTERP_WRAP);
-      interpolate(m_curHSV.sat, m_nextHSV.sat);
-      interpolate(m_curHSV.val, m_nextHSV.val);
-    } else {
-      if (m_curRGB == m_nextRGB) {
-        m_nextRGB = m_colorset.getNext();
-      }
-      interpolate(m_curRGB.red, m_nextRGB.red);
-      interpolate(m_curRGB.green, m_nextRGB.green);
-      interpolate(m_curRGB.blue, m_nextRGB.blue);
-    }
+      interpolate(m_cur.red, m_next.red);
+      interpolate(m_cur.green, m_next.green);
+      interpolate(m_cur.blue, m_next.blue);
   }
-
-  if (!m_flip) {
-    Leds::setIndex(m_ledPos, (m_hueShift ? (RGBColor)m_curHSV : m_curRGB));
-  } else {
-    HSVColor hsvCol = (m_hueShift ? m_curHSV : rgb_to_hsv_generic(m_curRGB));
+  RGBColor col = m_cur;
+  if (m_flip) {
+    // convert to hsv
+    HSVColor hsvCol = m_cur;
+    // shift the hue by a flip size
     hsvCol.hue += (m_flip * (255 / m_numFlips));
-    Leds::setIndex(m_ledPos, hsv_to_rgb_generic(hsvCol));
+    // convert the hsv color back to RGB
+    col = hsvCol;
   }
+  // set the color
+  Leds::setIndex(m_ledPos, col);
+  // increment the flip count
   m_flip++;
+  // modulate the flip count DO NOT USE MODULO OPERATOR BECAUSE
+  // THE FLIP COUNT COULD BE 0 THAT WILL DIVIDE BY ZERO
   if (m_flip >= m_numFlips) {
     m_flip = 0;
   }
 }
 
-void BlendPattern::interpolate(uint8_t &current, const uint8_t next, InterpWrapType wrap)
+void BlendPattern::interpolate(uint8_t &current, const uint8_t next)
 {
-  if (next == current) {
-    return;
+  if (current < next) {
+    uint8_t step = (next - current) > m_blendSpeed ? m_blendSpeed : (next - current);
+    current += step;
+  } else if (current > next) {
+    uint8_t step = (current - next) > m_blendSpeed ? m_blendSpeed : (current - next);
+    current -= step;
   }
-  // calculate the difference between the current and next value, this will calculate
-  // either the circular difference if INTERP_WRAP is set, or just the plain integer
-  // difference if it's not set. The circular difference accounts for wrapping past
-  // 255 <-> 0 to get to the next value quicker than the other direction
-  int diff = next - current;
-  if (wrap == INTERP_WRAP) {
-    // wrap around from 256 to 0 and interpolate through 0 if it's a closer path
-    diff = (int)(((uint8_t)((diff + 128 + 256) % 256)) - 128);
-  }
-  // the step will either be -1 or 1 based on the direction of the difference
-  int step = (diff > 0) ? 1 : -1;
-  // multiply the difference by the step, this effectively does diff = abs(diff)
-  diff *= step;
-  // now that we know diff is absolute (positive) we can compare it to the blend speed
-  if (diff > m_blendSpeed) {
-    // if the diff is large enough to fit the blendspeed then include it in the step
-    step += (step * m_blendSpeed);
-  }
-  // add the step to the current value to move towards the next value
-  current += step;
 }
--- VortexEngine/src/Patterns/Single/BlendPattern.h
+++ VortexEngine/src/Patterns/Single/BlendPattern.h
@@ -16,23 +16,15 @@ public:
 protected:
   // only override the onBlinkOn so we can control the color it blinks
   virtual void onBlinkOn() override;
-
-  enum InterpWrapType {
-    INTERP_NO_WRAP,
-    INTERP_WRAP
-  };
-  void interpolate(uint8_t &current, const uint8_t next, InterpWrapType wrap = INTERP_NO_WRAP);
+  void interpolate(uint8_t &current, const uint8_t next);
 
   // offset of starting hue and number of flips
   uint8_t m_blendSpeed;
   uint8_t m_numFlips;
-  uint8_t m_hueShift;
 
   // current color and target blend color
-  HSVColor m_curHSV;
-  HSVColor m_nextHSV;
-  RGBColor m_curRGB;
-  RGBColor m_nextRGB;
+  RGBColor m_cur;
+  RGBColor m_next;
 
   // the current flip counter
   uint8_t m_flip;
