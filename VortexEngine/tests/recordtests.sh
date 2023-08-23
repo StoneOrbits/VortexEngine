e71366641f00bfa270e27c1fc2d0b142a207513b
Dan <UnrealDanMail@gmail.com>
2023-08-22 23:59:44 -0700

    recordtests script is async now

--- VortexEngine/tests/recordtests.sh
+++ VortexEngine/tests/recordtests.sh
@@ -112,38 +112,8 @@ function record_tests() {
   TESTCOUNT=0
 
   for FILE in $FILES; do
-    INPUT="$(grep "Input=" $FILE | cut -d= -f2)"
-    BRIEF="$(grep "Brief=" $FILE | cut -d= -f2)"
-    ARGS="$(grep "Args=" $FILE | cut -d= -f2)"
-    TESTNUM="$(echo $FILE | cut -d/ -f2 | cut -d_ -f1 | cut -d/ -f2)"
-    TESTNUM=$((10#$TESTNUM))
+    ./record_test.sh $FILE $VALIDATE $TESTCOUNT $NUMFILES &
     TESTCOUNT=$((TESTCOUNT + 1))
-    echo -e -n "\e[31mRecording $PROJECT ($TESTCOUNT/$NUMFILES) \e[33m[\e[97m$BRIEF\e[33m] \e[33m[\e[97m$ARGS\e[33m]...\e[0m"
-    TEMP_FILE="tmp/${FILE}.out"
-    # Append the output of the $VORTEX command to the temp file
-    # NOTE: When recording the tests we don't use valgrind because
-    #       the valgrind output should be clean anyway. But when running
-    #       the test valgrind is used with --leak-check=full --show-leak-kinds=all
-    echo "Input=${INPUT}" > "$TEMP_FILE"
-    echo "Brief=${BRIEF}" >> "$TEMP_FILE"
-    echo "Args=${ARGS}" >> "$TEMP_FILE"
-    echo "--------------------------------------------------------------------------------" >> "$TEMP_FILE"
-    $VORTEX $ARGS --no-timestep --hex <<< $INPUT >> $TEMP_FILE
-    # strip any \r in case this was run on windows
-    sed -i 's/\r//g' $TEMP_FILE
-    # Replace the original file with the modified temp file
-    mv $TEMP_FILE $FILE
-    echo -e "\e[96mOK\e[0m"
-    # print out colorful if in verbose
-    if [ "$VALIDATE" -eq 1 ]; then
-      $VORTEX $ARGS --no-timestep --color <<< $INPUT
-      echo -e "\e[31mRecorded \e[33m[\e[97m$BRIEF\e[33m] \e[33m[\e[97m$ARGS\e[33m]\e[0m"
-      echo -en "${YELLOW}Is this correct? (Y/n):${WHITE} "
-      read -e CONFIRM
-      if [[ $CONFIRM == [nN] || $CONFIRM == [nN][oO] ]]; then
-        exit
-      fi
-    fi
   done
 
   #rm -rf tmp/$PROJECT
