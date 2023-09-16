721f4e19a5833c0753a6cfa190b695ce7a3afaa4
Dan <UnrealDanMail@gmail.com>
2023-09-16 15:39:29 -0700

    spi refactor

--- VortexEngine/src/Leds/Leds.cpp
+++ VortexEngine/src/Leds/Leds.cpp
@@ -14,7 +14,45 @@
 
 #ifdef VORTEX_EMBEDDED
 #include <SPI.h>
-#define POWER_LED_PIN 7
+#define ONBOARD_LED_SCK 8
+#define ONBOARD_LED_MOSI 7
+static void transfer(uint8_t byte)
+{
+  uint8_t startbit = 0x80;
+  bool lastmosi = !(byte & startbit);
+  for (uint8_t b = startbit; b != 0; b = b >> 1) {
+    delayMicroseconds(4);
+    bool towrite = byte & b;
+    if (lastmosi != towrite) {
+      digitalWrite(ONBOARD_LED_MOSI, towrite);
+      lastmosi = towrite;
+    }
+    digitalWrite(ONBOARD_LED_SCK, HIGH);
+    delayMicroseconds(4);
+    digitalWrite(ONBOARD_LED_SCK, LOW);
+  }
+}
+static void turnOffOnboardLED()
+{
+  // spi device begin
+  pinMode(ONBOARD_LED_SCK, OUTPUT);
+  digitalWrite(ONBOARD_LED_SCK, LOW);
+  pinMode(ONBOARD_LED_MOSI, OUTPUT);
+  digitalWrite(ONBOARD_LED_MOSI, HIGH);
+
+  // Begin transaction, setting SPI frequency
+  static const SPISettings mySPISettings(8000000, MSBFIRST, SPI_MODE0);
+  SPI.beginTransaction(mySPISettings);
+  for (uint8_t i = 0; i < 4; i++) {
+    transfer(0x00); // begin frame
+  }
+  transfer(0xFF); //  Pixel start
+  for (uint8_t i = 0; i < 3; i++) {
+    transfer(0x00); // R,G,B
+  }
+  transfer(0xFF); // end frame
+  SPI.endTransaction();
+}
 #endif
 
 // array of led color values
@@ -25,8 +63,8 @@ uint8_t Leds::m_brightness = DEFAULT_BRIGHTNESS;
 bool Leds::init()
 {
 #ifdef VORTEX_EMBEDDED
-  // turn off the power led don't need it for anything
-  pinMode(POWER_LED_PIN, INPUT_PULLUP);
+  turnOffOnboardLED();
+  SPI.begin();
 #endif
 #ifdef VORTEX_LIB
   Vortex::vcallbacks()->ledsInit(m_ledColors, LED_COUNT);
@@ -36,6 +74,9 @@ bool Leds::init()
 
 void Leds::cleanup()
 {
+#ifdef VORTEX_EMBEDDED
+  SPI.end();
+#endif
   for (uint8_t i = 0; i < LED_COUNT; ++i) {
     m_ledColors[i].clear();
   }
@@ -258,24 +299,25 @@ void Leds::holdAll(RGBColor col)
 void Leds::update()
 {
 #ifdef VORTEX_EMBEDDED
-  SPI.begin();
-  // Start frame
-  for (uint8_t i = 0; i < 4; i++) {
+  // the transaction prevents this from interfering with other communications
+  // on the pins that are used for SPI, for example IR is on pin 2
+  static const SPISettings mySPISettings(12000000, MSBFIRST, SPI_MODE0);
+  SPI.beginTransaction(mySPISettings);
+  // Double start frame, normally 4, idk why it's double
+  for (uint8_t i = 0; i < 8; i++) {
     SPI.transfer(0);
   }
+  // Adjust brightness to 5 bits
+  uint8_t adjustedBrightness = 0b11100000 | ((m_brightness >> 3) & 0b00011111);
   // LED frames
   for (LedPos pos = LED_FIRST; pos < LED_COUNT; pos++) {
-    // brightness is only 5 bits so shift m_brightness down, divide by 8 so it's within 0-31
-    SPI.transfer(0b11100000 | ((m_brightness >> 3) & 0b00011111)); // brightness
+    SPI.transfer(adjustedBrightness);     // brightness
     SPI.transfer(m_ledColors[pos].blue);  // blue
     SPI.transfer(m_ledColors[pos].green); // green
     SPI.transfer(m_ledColors[pos].red);   // red
   }
-  // End frame
-  for (uint8_t i = 0; i < 4; i++) {
-    SPI.transfer(0);
-  }
-  SPI.end();
+  // don't need to end the SPI frame apparently, just end transaction
+  SPI.endTransaction();
 #endif
 #ifdef VORTEX_LIB
   Vortex::vcallbacks()->ledsShow();
