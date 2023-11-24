fb3c52d4fbbbbd485bc4784cb32b695d8f7e05f0
Dan <UnrealDanMail@gmail.com>
2023-11-24 01:57:53 -0800

    fixed multi-led stuff and renamed materia to vortex

---
+++ VortexEngine/src/Patterns/Multi/VortexPattern.cpp
@@ -0,0 +1,41 @@
+#include "VortexPattern.h"
+
+#include "../../Serial/ByteStream.h"
+#include "../../Time/TimeControl.h"
+#include "../../Leds/Leds.h"
+#include "../../Log/Log.h"
+
+VortexPattern::VortexPattern(const PatternArgs& args) :
+  BlinkStepPattern(args),
+  m_progress(0)
+{
+  m_patternID = PATTERN_VORTEXWIPE;
+  setArgs(args);
+}
+
+VortexPattern::~VortexPattern()
+{
+}
+
+// init the pattern to initial state
+void VortexPattern::init()
+{
+  BlinkStepPattern::init();
+  // reset progress
+  m_progress = 0;
+  // start colorset at index 0 so cur() works
+  m_colorset.setCurIndex(0);
+}
+
+void VortexPattern::blinkOn()
+{
+  Leds::setRing((Ring)(RING_LAST - m_progress), m_colorset.peekNext());
+}
+
+void VortexPattern::poststep()
+{
+  m_progress = (m_progress + 1) % RING_COUNT;
+  if (RING_COUNT - m_progress == RING_LAST) {
+    m_colorset.getNext();
+  }
+}
