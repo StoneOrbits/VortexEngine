aa7a2738a7feac28f456376e0eb2ec1610cfdac2
Dan <UnrealDanMail@gmail.com>
2023-12-09 02:48:32 -0800

    ifdef'd out multi stuff

--- VortexEngine/src/Menus/MenuList/Randomizer.h
+++ VortexEngine/src/Menus/MenuList/Randomizer.h
@@ -27,10 +27,11 @@ public:
 private:
   // internal helper functions
   void showRandomizationSelect();
+#if VORTEX_SLIM == 0
   bool reRollMulti();
+#endif
   bool reRollSingles();
   bool reRollForContext(Random &ctx, LedPos pos);
-  bool splitMultiRandomize();
   bool applyPatternAndColorsetToMap(LedMap map, PatternID pattern, const Colorset &colorset);
   PatternID rollSingleLedPatternID(Random &ctx);
   PatternID rollMultiLedPatternID(Random &ctx);
