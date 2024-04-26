85b0a75ad3ada7cfa7ec1916e32a6ba1f02982b8
Unreal-Dan <72595612+Unreal-Dan@users.noreply.github.com>
2024-04-26 15:31:12 -0700

    Daniel/gloves/serialize harden (#232)
    * work for now
    
    * hardened all serialization code
    
    * hardened all serialize code
    
    * more hardening
    
    * fix wasm

--- VortexEngine/src/Modes/Modes.cpp
+++ VortexEngine/src/Modes/Modes.cpp
@@ -83,11 +83,11 @@ bool Modes::serializeSaveHeader(ByteStream &saveBuffer)
   }
   // NOTE: instead of global brightness the duo uses this to store the
   //       startup mode ID. The duo doesn't offer a global brightness option
-  if (!saveBuffer.serialize(m_globalFlags)) {
+  if (!saveBuffer.serialize8(m_globalFlags)) {
     return false;
   }
   // serialize the global brightness
-  if (!saveBuffer.serialize((uint8_t)Leds::getBrightness())) {
+  if (!saveBuffer.serialize8((uint8_t)Leds::getBrightness())) {
     return false;
   }
   DEBUG_LOGF("Serialized all modes, uncompressed size: %u", saveBuffer.size());
@@ -101,8 +101,12 @@ bool Modes::unserializeSaveHeader(ByteStream &saveHeader)
   uint8_t major = 0;
   uint8_t minor = 0;
   // unserialize the vortex version
-  saveHeader.unserialize(&major);
-  saveHeader.unserialize(&minor);
+  if (!saveHeader.unserialize8(&major)) {
+    return false;
+  }
+  if (!saveHeader.unserialize8(&minor)) {
+    return false;
+  }
   // check the version for incompatibility
   if (!VortexEngine::checkVersion(major, minor)) {
     // incompatible version
@@ -112,10 +116,14 @@ bool Modes::unserializeSaveHeader(ByteStream &saveHeader)
   // NOTE: instead of global brightness the duo uses this to store the
   //       startup mode ID. The duo doesn't offer a global brightness option
   // unserialize the global brightness
-  saveHeader.unserialize(&m_globalFlags);
+  if (!saveHeader.unserialize8(&m_globalFlags)) {
+    return false;
+  }
   // unserialize the global brightness
   uint8_t brightness = 0;
-  saveHeader.unserialize(&brightness);
+  if (!saveHeader.unserialize8(&brightness)) {
+    return false;
+  }
   if (brightness) {
     Leds::setBrightness(brightness);
   }
@@ -183,7 +191,9 @@ bool Modes::loadStorage()
   }
   // unserialize the number of modes next
   uint8_t numModes = 0;
-  headerBuffer.unserialize(&numModes);
+  if (!headerBuffer.unserialize8(&numModes)) {
+    return false;
+  }
   if (!numModes) {
     DEBUG_LOG("Did not find any modes");
     // this kinda sucks whatever they had loaded is gone
@@ -210,7 +220,7 @@ bool Modes::saveStorage()
     return false;
   }
   // serialize the number of modes
-  if (!headerBuffer.serialize(m_numModes)) {
+  if (!headerBuffer.serialize8(m_numModes)) {
     return false;
   }
   if (!Storage::write(0, headerBuffer)) {
@@ -233,7 +243,9 @@ bool Modes::saveStorage()
       return false;
     }
     // serialize it into the target modes buffer
-    mode->serialize(modeBuffer);
+    if (!mode->serialize(modeBuffer)) {
+      return false;
+    }
     // just uninstansiate the mode after serializing
     ptr->uninstantiate();
     // next mode
@@ -255,7 +267,7 @@ bool Modes::saveStorage()
 bool Modes::serialize(ByteStream &modesBuffer)
 {
   // serialize the number of modes
-  if (!modesBuffer.serialize(m_numModes)) {
+  if (!modesBuffer.serialize8(m_numModes)) {
     return false;
   }
   // make sure the current mode is saved in case it has changed somehow
@@ -273,7 +285,9 @@ bool Modes::serialize(ByteStream &modesBuffer)
       return false;
     }
     // serialize it into the target modes buffer
-    mode->serialize(modesBuffer);
+    if (!mode->serialize(modesBuffer)) {
+      return false;
+    }
     // just uninstansiate the mode after serializing
     ptr->uninstantiate();
     // next mode
@@ -296,7 +310,9 @@ bool Modes::unserialize(ByteStream &modesBuffer)
   clearModes();
   // unserialize the number of modes next
   uint8_t numModes = 0;
-  modesBuffer.unserialize(&numModes);
+  if (!modesBuffer.unserialize8(&numModes)) {
+    return false;
+  }
   if (!numModes) {
     DEBUG_LOG("Did not find any modes");
     // this kinda sucks whatever they had loaded is gone
