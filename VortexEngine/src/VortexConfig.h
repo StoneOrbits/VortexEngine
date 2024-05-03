145291a8710f200cf838db0bdfcf44d406c3ca72
Dan <UnrealDanMail@gmail.com>
2024-05-02 23:23:23 -0700

    fix

--- VortexEngine/src/VortexConfig.h
+++ VortexEngine/src/VortexConfig.h
@@ -7,14 +7,18 @@
 // The engine major version indicates the state of the save file,
 // if any changes to the save format occur then the major version
 // must increment so that the savefiles will not be loaded
+#ifndef VORTEX_VERSION_MAJOR
 #define VORTEX_VERSION_MAJOR  1
+#endif
 
 // A minor version simply indicates a bugfix or minor change that
 // will not effect the save files produces by the engine. This means
 // a savefile produced by 1.1 should be loadable by an engine on 1.2
 // and vice versa. But an engine on 2.0 cannot share savefiles with
 // either of the engines on version 1.1 or 1.2
+#ifndef VORTEX_VERSION_MINOR
 #define VORTEX_VERSION_MINOR  1
+#endif
 
 // The build or patch number based on the major.minor version, this is
 // set by the build system using the number of commits since last version
@@ -23,7 +27,9 @@
 #endif
 
 // produces a number like 1.0
+#ifndef VORTEX_VERSION_NUMBER
 #define VORTEX_VERSION_NUMBER VORTEX_VERSION_MAJOR.VORTEX_VERSION_MINOR.VORTEX_BUILD_NUMBER
+#endif
 
 // produces a string like "1.1.0"
 #define ADD_QUOTES(str) #str
