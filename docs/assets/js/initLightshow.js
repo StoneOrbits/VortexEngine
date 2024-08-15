a50612619d8840c7c189955341d597c2bf21ec30
Dan <UnrealDanMail@gmail.com>
2024-08-15 00:42:17 -0700

    improved lightshow

--- docs/assets/js/initLightshow.js
+++ docs/assets/js/initLightshow.js
@@ -3,7 +3,7 @@ import Lightshow from './Lightshow.js';
 
 document.addEventListener('DOMContentLoaded', function() {
   const canvasId = 'vortex-canvas';
-  const options = { type: 'scrolling', sectionCount: 20 };
+  const options = { type: 'flashing', sectionCount: 20 };
 
   VortexLib().then(function(vortexLib) {
     const lightshow = new Lightshow(vortexLib, canvasId, options);
